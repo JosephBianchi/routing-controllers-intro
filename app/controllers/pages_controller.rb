9e47518b83a1549767bd71bb60251b4e9064d80c
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-30 15:31:07 -0400

    gave instance variable value through about, contest, and welcome classes in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,17 @@ class PagesController < ApplicationController
   # def welcome
   #   render :about
   # end
+  def welcome
+    @header = "This is the welcome page"
+  end
+
+  def about
+    @header = "This is the about page"
+  end
+
+  def contest
+    @header = "This is the contest page"
+  end
 
 
 
